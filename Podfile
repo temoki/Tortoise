0c4c4c3c3430698d2c21f244f2638fca09ecddcd
temoki <temoki@gmail.com>
2016-08-10 13:59:51 +0900

    setup pods

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '10.0'
+
+target 'TortoiseDemo' do
+  use_frameworks!
+end
---
+++ Pods/Target Support Files/Pods-TortoiseDemo/Pods-TortoiseDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TortoiseDemo : NSObject
+@end
+@implementation PodsDummy_Pods_TortoiseDemo
+@end
