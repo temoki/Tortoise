b953481d5a8831defcd8165674da0f4c7c17659a
temoki <temoki@gmail.com>
2016-08-10 14:02:03 +0900

    Add library target

---
+++ Tortoise/Tortoise.h
@@ -0,0 +1,19 @@
+//
+//  Tortoise.h
+//  Tortoise
+//
+//  Created by temoki on 2016/08/10.
+//  Copyright © 2016年 temoki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tortoise.
+FOUNDATION_EXPORT double TortoiseVersionNumber;
+
+//! Project version string for Tortoise.
+FOUNDATION_EXPORT const unsigned char TortoiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tortoise/PublicHeader.h>
+
+
