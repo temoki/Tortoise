f4d37b7d1384386f0655527c02ded8325b14f5bd
temoki <temoki@gmail.com>
2016-08-10 13:57:18 +0900

    Create demo application project

---
+++ TortoiseDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TortoiseDemo
+//
+//  Created by temoki on 2016/08/10.
+//  Copyright © 2016年 temoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
