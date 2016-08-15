879489c9503c7cca7af163bd03d16a4c3c8ad1c7
temoki <temoki@gmail.com>
2016-08-16 00:02:36 +0900

    Update demo

--- TortoiseDemo/Canvas.swift
+++ TortoiseDemo/Canvas.swift
@@ -17,12 +17,28 @@ class Canvas: UIView {
                             canvasWidth: self.bounds.size.width,
                             canvasHeight: self.bounds.size.height)
 
-        🐢.SetPenWidth(0.5)
-            .SetPenColor(3)
-            .Repeat(400) {
-                🐢.Repeat(34) {
-                    🐢.Forward(12).Right(10)}.Right(90)
-            }.Run()
+        🐢.SetRGB(0, [0.8, 0.8, 0.8])
+            .Repeat(12) { 🐢
+                .SetPenWidth(2)
+                .Right(15)
+                .Repeat(6) { 🐢
+                    .SetPenColor(.PenColor({ $0 + 1 }))
+                    .Forward(50)
+                    .Right(60)
+                }
+                .SetPenWidth(1)
+                .Right(15)
+                .Repeat(6) { 🐢
+                    .SetPenColor(.PenColor({ $0 + 1 }))
+                    .Forward(20)
+                    .Right(60)
+                }
+            }
+            .SetPenWidth(3)
+            .SetPenColor(0)
+            .Back(150)
+            .SetPenColor(1)
+            .Run()
 
     }
 
